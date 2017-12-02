84f0174e754a743513f6c787f6fffb83f0a3ba1f
VECTOR\Ethan <ejohn121@msudenver.edu>
2017-12-02 12:35:50 -0700

    updated syntax

--- src/Meta.java
+++ src/Meta.java
@@ -7,7 +7,6 @@ public class Meta {
     private static final int NAME_LENGTH = 10;
 
     public static void main(String[] args) {
-
         if (args.length == 0) {
             args = new String[] {"Hello!"};
         }
@@ -23,29 +22,25 @@ public class Meta {
 
             File file = new File(randomName.toString());
 
+            System.out.println("");
+
             writeProgram(file,args[i]);
-            System.out.println("");
-
             compileProgram(file);
-            System.out.println("");
-
             runProgram(file);
         }
     }
 
     private static void writeProgram(File file, String msg) {
-
-        System.out.println("\nWriting " + file + " ...");
+        System.out.println("Writing " + file + " ...");
 
         try {
             FileWriter out = new FileWriter(file);
             String className = file.getName().split(".java")[0];
 
             StringBuilder contents = new StringBuilder();
-
             contents.append("public class %s {\n");
             contents.append("\tpublic static void main(String[] args) {\n");
-            contents.append("\t\tSystem.out.println(\"Hello, world! My name is: ");
+            contents.append("\t\tSystem.out.println(\"Hello, world! My name is ");
             contents.append(className + " and I have come to say... " + msg + "\");");
             contents.append("\n\t}\n}\n");
 
@@ -59,11 +54,10 @@ public class Meta {
     }
 
     private static void compileProgram(File file) {
-
-        System.out.println("Compiling " + file);
+        System.out.println("Compiling " + file + " ...");
 
         try {
-            System.out.println(file.getAbsoluteFile());
+            //System.out.println(file.getAbsoluteFile());
             String[] cmd = getCmd("javac " + file.getName());
             Process pro = Runtime.getRuntime().exec(cmd);
             pro.waitFor();
@@ -73,68 +67,58 @@ public class Meta {
             e.printStackTrace();
         }
     }
-    //
-    // public static String className(File file) {
-    //     return file.getName().split(".java")[0];
-    // }
-
-    private static String getPackage() {
-        try {
-            return Class.forName("Meta").getClass().getPackage().toString();
-        } catch (Exception e) {
-            System.err.println("Unable to get package name");
-            e.printStackTrace();
-        }
-        return "unknown";
-    }
 
     private static void runProgram(File file) {
-        System.out.println("Running " + file);
+        System.out.println("Running " + file + " ...");
+        System.out.println("");
+
         String className = file.getName().split(".java")[0];
+
         try {
             String[] cmd = getCmd("java " + className);
+
             Process pro = Runtime.getRuntime().exec(cmd);
             BufferedReader in = new BufferedReader(new InputStreamReader(pro.getInputStream()));
+
             String line;
             while ((line = in.readLine()) != null) {
                 System.out.println("Program standard out: " + line);
             }
+
             pro.waitFor();
-        } catch (Exception e) {
+        }
+        catch (Exception e) {
             System.err.println("Error occured when attempting to run " + file);
             e.printStackTrace();
         }
     }
 
     private static String[] getCmd(String args) {
-        String osName = System.getProperty("os.name" );
+        String osName = System.getProperty("os.name");
+
         String[] cmd = new String[3];
-        if( osName.equals( "Windows NT" ) || osName.equals("Windows 10") )
-        {
+        if (osName.equals("Windows NT") || osName.equals("Windows 10")) {
             cmd[0] = "cmd.exe" ;
             cmd[1] = "/C start cmd" ;
             cmd[2] = args;
         }
-        else if ( osName.equals("Windows 8.1")) {
+        else if (osName.equals("Windows 8.1")) {
             cmd[0] = "cmd";
             cmd[1] = "/c";
             cmd[2] = args;
         }
-        else if( osName.equals( "Windows 95" ) )
-        {
+        else if (osName.equals("Windows 95")) {
             cmd[0] = "command.com" ;
             cmd[1] = "/C" ;
             cmd[2] = args;
-        } else {
+        }
+        else { // use UNIX / LINUX
             System.out.println(osName);
             cmd[0] = "/bin/sh";
             cmd[1] = "-c";
             cmd[2] = args;
         }
+
         return cmd;
     }
-
-    // private static String separator() {
-    //     return System.getProperty("file.separator");
-    // }
 }
